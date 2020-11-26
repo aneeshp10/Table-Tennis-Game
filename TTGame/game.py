98cf0b2492a919c2d2d9e90780d304368b0cf88b
aneeshp10 <56106124+aneeshp10@users.noreply.github.com>
2020-11-27 03:41:19 +0530

    Formatted

--- TTGame/game.py
+++ TTGame/game.py
@@ -1,17 +1,17 @@
 import turtle
 
-wn = turtle.Screen()
-wn.title("Aneesh P")
-wn.bgcolor("blue")
-wn.setup(width=800, height= 600)
-wn.tracer(0)
+game = turtle.Screen()
+game.title("Welcome to Python Table Tennis!")
+game.bgcolor("blue")
+game.setup(width=800, height=600)
+game.tracer(0)
 
 # Paddle One
 paddleOne = turtle.Turtle()
 paddleOne.speed(0)
 paddleOne.shape("square")
 paddleOne.color("green")
-paddleOne.shapesize(stretch_wid=5, stretch_len= 1)
+paddleOne.shapesize(stretch_wid=5, stretch_len=1)
 paddleOne.penup()
 paddleOne.goto(-350, 0)
 
@@ -20,7 +20,7 @@ paddleTwo = turtle.Turtle()
 paddleTwo.speed(0)
 paddleTwo.shape("square")
 paddleTwo.color("green")
-paddleTwo.shapesize(stretch_wid=5, stretch_len= 1)
+paddleTwo.shapesize(stretch_wid=5, stretch_len=1)
 paddleTwo.penup()
 paddleTwo.goto(350, 0)
 
@@ -41,48 +41,56 @@ score = turtle.Turtle()
 score.color("white")
 score.penup()
 score.hideturtle()
-score.goto(0,255)
-score.write("Player 1: 0                                            Player 2: 0", align="center", font=("Georgia", 24, "bold"))
+score.goto(0, 255)
+score.write("Player 1: 0                                            Player 2: 0",
+            align="center", font=("Georgia", 24, "bold"))
 
 winner = turtle.Turtle()
 winner.hideturtle()
 winner.penup()
-winner.goto(0,-50)
+winner.goto(0, -50)
 winner.color("white")
 
 # left bat going up
+
+
 def paddleOneUp():
     y = paddleOne.ycor()
     y = y + 30
     paddleOne.sety(y)
 
 # left bat going down
+
+
 def paddleOneDown():
     y = paddleOne.ycor()
     y = y - 30
     paddleOne.sety(y)
+
 
 def paddleTwoUp():
     y = paddleTwo.ycor()
     y = y + 30
     paddleTwo.sety(y)
 
+
 def paddleTwoDown():
     y = paddleTwo.ycor()
     y = y - 30
     paddleTwo.sety(y)
 
-# Keyboard binding
-wn.listen()
-wn.onkeypress(paddleOneUp, "w")
-wn.onkeypress(paddleOneDown, "s")
 
-wn.onkeypress(paddleTwoUp, "Up")
-wn.onkeypress(paddleTwoDown, "Down")
+# Keyboard binding
+game.listen()
+game.onkeypress(paddleOneUp, "w")
+game.onkeypress(paddleOneDown, "s")
+
+game.onkeypress(paddleTwoUp, "Up")
+game.onkeypress(paddleTwoDown, "Down")
 
 
 while True:
-    wn.update()
+    game.update()
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
@@ -96,41 +104,45 @@ while True:
         ball.dy = ball.dy * -1
 
     if ball.xcor() > 390:
-        playerOne+=1
+        playerOne += 1
         score.clear()
-        if (playerOne == 7) :
-            winner.write("Player One Wins", align="center", font=("Georgia", 24, "normal"))
+        if (playerOne == 7):
+            winner.write("Player One Wins", align="center",
+                         font=("Georgia", 24, "normal"))
             ball.color("blue")
             ball.dx = 0
             ball.dy = 0
-        
-        score.write("Player 1: {}                                            Player 2: {}".format(playerOne, playerTwo), align="center", font=("Georgia", 24, "bold"))
-        ball.goto(0,0)
+
+        score.write("Player 1: {}                                            Player 2: {}".format(
+            playerOne, playerTwo), align="center", font=("Georgia", 24, "bold"))
+        ball.goto(0, 0)
         ball.dx = ball.dx * -1
 
     if ball.xcor() < -390:
-        playerTwo+=1
+        playerTwo += 1
         score.clear()
-        if (playerTwo == 7) :
-            winner.write("Player Two Wins", align="center", font=("Georgia", 24, "normal"))
+        if (playerTwo == 7):
+            winner.write("Player Two Wins", align="center",
+                         font=("Georgia", 24, "normal"))
             ball.color("blue")
             ball.dx = 0
             ball.dy = 0
 
-        score.write("Player 1: {}                                            Player 2: {}".format(playerOne, playerTwo), align="center", font=("Georgia", 24, "bold"))
-        
-        ball.goto(0,0)
+        score.write("Player 1: {}                                            Player 2: {}".format(
+            playerOne, playerTwo), align="center", font=("Georgia", 24, "bold"))
+
+        ball.goto(0, 0)
         ball.dx = ball.dx * -1
 
     # Collisions
-    if (ball.xcor() > 340) :
-        if (ball.xcor() < 350) :
-            if (paddleTwo.ycor() - 40 < ball.ycor() < paddleTwo.ycor() + 40) :
-                ball.setx(340)
+    if (ball.xcor() < -340):
+        if (ball.xcor() > -350):
+            if (paddleOne.ycor() - 40 < ball.ycor() < paddleOne.ycor() + 40):
+                ball.setx(-340)
                 ball.dx = ball.dx * - 1
 
-    if (ball.xcor() < -340) :
-        if (ball.xcor() > -350) :
-            if (paddleOne.ycor() - 40 < ball.ycor() < paddleOne.ycor() + 40) :
-                ball.setx(-340)
+    if (ball.xcor() > 340):
+        if (ball.xcor() < 350):
+            if (paddleTwo.ycor() - 40 < ball.ycor() < paddleTwo.ycor() + 40):
+                ball.setx(340)
                 ball.dx = ball.dx * - 1
